fb7491e5964fc38629b58dd562abbbe71958cfb5
ningxin21 <jesse@wokoworks.com>
2022-04-24 17:37:05 +0800

    check bech32 length

--- types/bech32/bech32.go
+++ types/bech32/bech32.go
@@ -18,8 +18,16 @@ func ConvertAndEncode(hrp string, data []byte) (string, error) {
 
 // DecodeAndConvert decodes a bech32 encoded string and converts to base64 encoded bytes.
 func DecodeAndConvert(bech string) (string, []byte, error) {
+	if len(bech) != 42 {
+		return prefix, nil, errors.New(fmt.Sprintf("invalid address length, expected: %d, got: %d", 42, len(bech)))
+	}
 	if !strings.HasPrefix(bech, prefix) {
 		return prefix, nil, errors.New(fmt.Sprintf("invalid address prefix, expected %s", prefix))
 	}
-	return prefix, common.HexToAddress(bech).Bytes(), nil
+	ethAddress := common.HexToAddress(bech)
+	commonAddress := common.Address{}
+	if ethAddress == commonAddress {
+		return prefix, nil, errors.New(fmt.Sprintf("invalid address"))
+	}
+	return prefix, ethAddress.Bytes(), nil
 }
